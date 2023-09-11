315fb6007437da385b296d4d63becb60613c09a4
Rockey Kumar <104070946+rockeykumarmuz@users.noreply.github.com>
2023-09-11 19:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function threeSum(arr, target) {
 // write your code here
+
+	arr.sort((a, b)=>{
+		return a - b;
+	});
+	
+	let min = Infinity;
+	let ans = 0;
+
+    let n = arr.length;
+
+	for(let i=0; i<n; i++) {
+		let s = i+1 ;
+	    let e = n-1;
+
+		while(s<e) {
+			let sum = arr[i] + arr[s] + arr[e];
+
+			if(sum == target) {
+				return sum;
+            }
+
+			if(Math.abs(min)>Math.abs(target - sum)) {
+				min = target-sum;
+				ans = sum;
+			} else if(sum>target) {
+				e--;
+			} else {
+               s++;
+			}
+        }
+	}
+
+	return ans;
   
 }
 
